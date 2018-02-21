84503516d9bff47fb2d31f9d9c0a44461dfbb3c9
Timson020 <a_sa_f@126.com>
2018-02-22 01:10:37 +0800

    remove the folder

--- tools/setup.js
+++ tools/setup.js
@@ -12,7 +12,7 @@ const android_path = `${_path}android_back`
 
 const appName = argvs[2]
 
-// 返回Stats对象
+// function for check the path
 async function isPath(path) {
 	return new Promise((res) => {
 		fs.lstat(path, (err, d) => {
@@ -22,23 +22,37 @@ async function isPath(path) {
 	})
 }
 
+// function for del the folder
+function deleteFolder(path) {
+	if (fs.existsSync(path)) {
+		fs.readdirSync(path).forEach((file) => {
+			const curPath = `${path}/${file}`
+			if (fs.statSync(curPath).isDirectory()) { // recurse
+				deleteFolder(curPath)
+			} else { // delete file
+				fs.unlinkSync(curPath)
+			}
+		})
+		fs.rmdirSync(path)
+	}
+}
+
 // 开始
 async function start() {
+	// del the folder
 	const ios = await isPath(ios_path)
 	const android = await isPath(android_path)
 	// const androidStats = android.data
 	// const iosStats = ios.data
 	if (ios.code == 200) {
-		console.info('ios')
-		fs.unlinkSync(ios_path)
+		deleteFolder(ios_path)
 		console.info('ios_path is success delete')
 	}
 	if (android.code == 200) {
-		console.info('android')
-		fs.unlinkSync(android_path)
-
+		deleteFolder(android_path)
 		console.info('android_path is success delete')
 	}
+	console.info(appName)
 }
 
 start()
