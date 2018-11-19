9a837385f5a309fa69d2da6968e25d50765aa85b
Timson020 <a_sa_f@126.com>
2018-11-19 15:14:56 +0800

    modify the cmd

--- tools/setup.js
+++ tools/setup.js
@@ -81,8 +81,7 @@ async function start() {
 
 	console.info('\x1B[32m', '----- success, It is all complete -----\n\n')
 	console.info('\x1B[33m', '***** please run step by step on cmd *****\n')
-	console.info('\x1B[40m', `>>>>>>> mkdir Ex && cd ./Ex && react-native init ${appName}\n`)
-	console.info('\x1B[40m', `>>>>>>> mv ./${appName}/ios ../ios && mv ./${appName}/android ../android && cd .. && rm -fr Ex\n`)
+	console.info('\x1B[40m', `>>>>>>> mkdir Ex && cd ./Ex && react-native init ${appName} && mv ./${appName}/ios ../ios && mv ./${appName}/android ../android && cd .. && rm -fr Ex\n`)
 	console.info('\x1B[40m', '>>>>>>> npm run new\n')
 	console.info('\x1B[40m', `!!!!! please check the ./${appName}/package.json, react && react-native version, to modify ./package.json`)
 }
